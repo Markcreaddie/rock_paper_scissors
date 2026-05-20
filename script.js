3463d2365d845221b0043edc3d691a660f36762a
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2026-05-20 20:03:30 +0300

    Create getHumanChoice function
    Uses the prompt method to get the user's input
    of either 'rock', 'paper' or 'scissors'

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ console.log("Hello world")
 function getComputerChoice() {
     let options = ["rock", "paper", "scissors"]
     let makeChoice = (options) => {
-        randindex = Math.floor(Math.random() * options.length)
-        return (options[randindex])
+        randval = Math.floor(Math.random() * options.length)
+        return (options[randval])
     }
 
     return makeChoice(options)
+}
+
+function getHumanChoice() {
+    let choice = prompt("Select 'rock', 'paper' or 'scissors'")
+    return choice
 }
 
 console.log(getComputerChoice())
